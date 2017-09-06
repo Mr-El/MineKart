a3e59069ab5b8f2efa9bcbf08c6b02aa304e8721
Mr. El <elanebritton96@gmail.com>
2017-09-06 14:15:56 -0400

    Deleted Desktop App
    Due to the fact that JQuery is not supported in a Chrome app, the app will be deleted and started over.

--- MineKart_app/background.js
+++
@@ -1,8 +0,0 @@
-﻿chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('SplashScreen.html', {
-        'outerBounds': {
-            'width': 400,
-            'height': 500
-        }
-    });
-});
