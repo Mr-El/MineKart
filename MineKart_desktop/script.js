05e456c2b9fddac7caa7787808e29c75da09982e
Mr. El <elanebritton96@gmail.com>
2017-09-06 20:43:15 -0400

    Updated for StackOverFlow

--- MineKart_desktop/script.js
+++ MineKart_desktop/script.js
@@ -1,1 +1,4 @@
-﻿
+﻿document.querySelector('#startButton').addEventListener('click', function () {
+    chrome.app.window.create('index.html', { "width": 400, "height": 500 });
+    window.close();
+});
