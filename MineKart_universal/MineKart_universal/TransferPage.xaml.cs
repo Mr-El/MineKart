b4a3fa6689a7e7bd9354754752306318ddcadaf5
Mr. El <elanebritton96@gmail.com>
2017-09-21 21:34:00 -0400

    bunch of changes

--- MineKart_universal/MineKart_universal/TransferPage.xaml.cs
+++ MineKart_universal/MineKart_universal/TransferPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -36,7 +24,7 @@ namespace MineKart_universal
         {
             if (StartListBoxItem.IsSelected)
             {
-                this.Frame.Navigate(typeof(TransferPage));
+                //this.Frame.Navigate(typeof(TransferPage));
             }
 
             if (FolderListBoxItem.IsSelected)
@@ -48,6 +36,12 @@ namespace MineKart_universal
             {
                 this.Frame.Navigate(typeof(SavePage));
             }
+        }
+
+        private void Button_Click(System.Object sender, RoutedEventArgs e)
+        {
+            SocketTransferServer youtube = new Class1();
+            
         }
     }
 }
